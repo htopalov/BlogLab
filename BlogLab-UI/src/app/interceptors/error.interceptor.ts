c3ca8d11e61b9aa4361c264cf267847ce85b888f
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-08-24 13:47:34 +0300

    implement interceptors

--- BlogLab-UI/src/app/interceptors/error.interceptor.ts
+++ BlogLab-UI/src/app/interceptors/error.interceptor.ts
@@ -5,14 +5,83 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../services/account.service';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(
+    private toastr: ToastrService,
+    private accountService: AccountService
+  ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError(error => {
+        if (error) {
+          switch(error.status){
+            case 400:
+              this.handle400Error(error);
+              break;
+            case 401:
+              this.handle401Error(error);
+              break;
+            case 500:
+              this.handle500Error(error);
+              break;
+            default:
+              this.handleUnexpectedError(error);
+              break;
+          }
+        }
+        return throwError(error);
+      })
+    );
+  }
+
+  handle400Error(error: any){
+    if (!!error.error && Array.isArray(error.error)) {
+      let errorMessage = '';
+      for(const key in error.error){
+        if (!!error.error[key]) {
+          const errorElement = error.error[key];
+          errorMessage = (`${errorMessage}${errorElement.code} - ${errorElement.description}\n`);
+        }
+      }
+      this.toastr.error(errorMessage,error.statusText);
+    } else if(!!error?.error.errors?.Content && (typeof error.error.errors.Content) === 'object'){
+      let errorObject = error.error.errors.Content;
+      let errorMessage = '';
+      for(const key in errorObject){
+        const errorElement = errorObject[key];
+        errorMessage = (`${errorMessage}${errorElement}\n`);
+      }
+      this.toastr.error(errorMessage, error.statusCode);
+    } else if(!!error.error){
+      let errorMessage = ((typeof error.error) == 'string')
+       ? error.error
+       : 'There was a validation error.';
+       this.toastr.error(errorMessage, error.statusCode);
+    } else {
+      this.toastr.error(error.statusText, error.status);
+    }
+  }
+
+  handle401Error(error: any){
+    let errorMessage = 'Please login to your account!';
+    this.accountService.logout();
+    this.toastr.error(errorMessage, error.statusText);
+    //todo: route to login page...
+  }
+
+  handle500Error(error: any){
+    this.toastr.error('Please contact admin. Error occured in server');
+  }
+
+  handleUnexpectedError(error: any){
+    this.toastr.error('Something went wrong :(');
   }
 }
